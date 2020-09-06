391c15139b14357bd18bd785cf1796fa489949f8
JaronArmiger <jaron.armiger@gmail.com>
2020-09-06 18:07:03 -0500

    configure nconf to read from config.json

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var nconf = require('nconf');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.locals.title = nconf.get('fuck');
+  res.render('index');
 });
 
 module.exports = router;
