4bb8c1cef66bcfa57da03b89b6e2a3248662b93c
JaronArmiger <jaron.armiger@gmail.com>
2020-09-08 19:19:40 -0500

    implement check for matching passwords

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,6 +11,17 @@ exports.signup_post = [
   validator.body('first_name', 'first name required').trim().isLength({ min: 1 }),
   validator.body('last_name', 'last name required').trim().isLength({ min: 1 }),
   validator.body('username', 'username required').trim().isLength({ min: 1 }),
+  validator.body('password', 'password must be 8-20 characters in length')
+    .trim().isLength({ min: 8, max: 20 }),
+  validator.body('password_confirmation', 'confirm password').trim().isLength({ min: 1 }),
+  validator.body('password_confirmation')
+    .trim().custom((value, { req }) => {
+      if (value !== req.body.password) {
+      	throw new Error('Passwords must match');
+      }
+      return true;
+    }),
+
   validator.sanitizeBody('first_name').escape(),
   validator.sanitizeBody('last_name').escape(),
   validator.sanitizeBody('username').escape(),
