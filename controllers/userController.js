9b12c68571f2a206772ce22eda9f770132a411f7
JaronArmiger <jaron.armiger@gmail.com>
2020-09-08 19:09:40 -0500

    implement display of validation errors on user_signup view

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,5 +8,17 @@ exports.signup_get = (req, res, next) => {
 };
 
 exports.signup_post = [
-  
+  validator.body('first_name', 'first name required').trim().isLength({ min: 1 }),
+  validator.body('last_name', 'last name required').trim().isLength({ min: 1 }),
+  validator.body('username', 'username required').trim().isLength({ min: 1 }),
+  validator.sanitizeBody('first_name').escape(),
+  validator.sanitizeBody('last_name').escape(),
+  validator.sanitizeBody('username').escape(),
+
+  (req, res, next) => {
+  	const errors = validator.validationResult(req);
+  	if (!errors.isEmpty()) {
+  	  res.render('user_signup', { title: 'User Signup', errors: errors.array() })
+  	}
+  }
 ];
